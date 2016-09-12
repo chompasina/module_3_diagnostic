6ebca5afbd5b8b3d4984fa81b20213d64b852747
Tommasina <tommasina1@gmail.com>
2016-09-12 10:29:17 -0600

    refactoring

--- app/services/nrel_client_service.rb
+++ app/services/nrel_client_service.rb
@@ -1,9 +1,28 @@
 class NrelClientService
   def initialize
-    @_conn = Faraday.new(:url => 'https://developer.nrel.gov/api/alt-fuel-stations/v1.json') do |faraday|
-      faraday.adapter       Faraday.default_adapter
-      faraday.params['api_key'] = ENV['NREL_API_KEY']
-    end
+    @_conn = Faraday.new(:url => 'https://developer.nrel.gov/') do |faraday|
+      faraday.adapter               Faraday.default_adapter
+      faraday.params['api_key']   = ENV['NREL_API_KEY']
+      faraday.params['fuel_type'] = 'LPG,ELEC'
+      faraday.params['limit']     = '10'
+      faraday.params['radius']    = '5.0'
+  end
+    # response = conn.get 'api/alt-fuel-stations/v1.json/?zip=80203' #, { :zip =>  }
+    # response.body
   end
   
+  def stations(zipcode)
+    parse(conn.get"api/alt-fuel-stations/v1.json/?zip=#{zipcode}")
+  end
+
+    
+  private
+
+    def parse(response)
+      JSON.parse(response.body, symbolize_names: true)
+    end
+    
+    def conn
+      @_conn
+    end
 end
